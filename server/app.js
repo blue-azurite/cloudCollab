d54522bbb90ba79b153f195648f2984e1720ee30
zoharsanchez <lorena.zohar@gmail.com>
2016-08-09 16:57:14 -0700

    Add socket.io to server

--- server/app.js
+++ server/app.js
@@ -1,16 +1,21 @@
 import express from 'express';
 import path from 'path';
 import db from './config/db';
+import http, { Server } from 'http';
+import Io from 'socket.io';
 
 const app = express();
+const server = Server(app);
+const io = Io(server);
 const port = process.env.PORT || 3000;
 
 // Render static index route
 app.use(express.static(path.join(__dirname,'../compiled')));
 app.use(express.static(path.join(__dirname,'../client')));
 
+io.on('connection', socket => console.log('Connected on the server side!'));
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log('----------------------------------------');
   console.log(`| app.js has been served on port: ${port} |`);
   console.log('----------------------------------------');
