1333b1349e8a47f5b39c19bc5f78ae19c60ccd60
juliusbuckley <juliusbuckley@gmail.com>
2016-08-12 14:46:44 -0700

    feat/ Created query to get user repos.

--- server/routes/apiRoutes.js
+++ server/routes/apiRoutes.js
@@ -1,7 +1,7 @@
 import passport from 'passport';
 import GitHubStrategy, { Strategy } from 'passport-github2';
-import { requestGithub } from '../github/githubQuery';
-
+import { requestGithub } from '../github/githubQueries';
+import request from 'request';
 export default function(app) {
   app.get('/api/github', 
     passport.authenticate('github', {scope: ['repo','user:email']})
@@ -21,8 +21,30 @@ export default function(app) {
       // console.log(req.user)
       res.redirect('/');
     });
+  app.get('/api/github/repos', (req, res) => {
+    let access_token = req.user.accessToken;
+    let user = req.user.username;
+    let url = `https://api.github.com/users/${user}/repos?access_token=${access_token}`;
+
+    let options = {
+      url: url,
+      headers: {
+        'User-Agent': user
+      }
+    };
+
+    const cb = (err, res, body) => {
+      console.log(body);
+    };
+
+    request(options,cb);
+  });
+
   app.get('/test', (req,res) => {
     console.log(req.user)
     res.send('hello');
   });
 };  
+
+
+// GET https://api.github.com/user/repos?access_token:
